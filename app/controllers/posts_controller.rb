cd1e69356236c004eaf3b42a0c6f5fdeba2dc0f6
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-04-25 16:14:20 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,9 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new
-      @post.title = params[:title]
-      @post.description = params[:description]
-      @post.save
-      redirect_to post_path(@post)
-end
-
-end
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
+ end
